22853fd79f0490dc166daf5e7d4dae73ce2c810c
System Administrator <alegrance1000@gmail.com>
2022-10-24 13:40:39 -0300

    Marcador frontend

--- src/app/pages/marcador/marcador.page.ts
+++ src/app/pages/marcador/marcador.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./marcador.page.scss'],
 })
 export class MarcadorPage implements OnInit {
+  public texto = "";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick(event: any) {
+    const text = event.target.innerText;
+
+    this.texto += text;
+
+    ((<HTMLInputElement>document.getElementById("input")).value) = this.texto;
+  }
+
+  borrar() {
+    this.texto = this.texto.slice(0, this.texto.length - 1);
+
+    ((<HTMLInputElement>document.getElementById("input")).value) = this.texto;
+  }
+
+  enviar() {
+    console.log(this.texto);
+  }
 }
